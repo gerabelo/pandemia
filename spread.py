d2273ae6727170031cee050307cbbfa6180c1afe
unknown <=>
2020-03-16 14:36:14 -0400

    alteracoes nos comentarios

--- spread.py
+++ spread.py
@@ -6,7 +6,7 @@
 # http://cadernos.ensp.fiocruz.br/csp/artigo/999/emergncia-do-novo-coronavrus-sars-cov-2-e-o-papel-de-uma-vigilncia-nacional-em-sade-oportuna-e-efetiva
 #
 # 2020: o Amazonas possui aproximadamente 1400 leitos UTI
-#
+# 2020: Manaus possui aproximadamente 2 milhões de habitantes
 
 f=open("covid19.csv","w+")
 f.write("\"weeks\",\"naive\",\"sick\",\"recovered\",\"hospitalized\",\"R0\"\n")
@@ -14,17 +14,15 @@ f.write("\"weeks\",\"naive\",\"sick\",\"recovered\",\"hospitalized\",\"R0\"\n")
 # R0 = [1.6,2.1,2.6,3.1,3.6,4.1] # Os dados observados pelos chineses sugerem um R0 variando de 1,6 a 4,1
 R0 = [1.029,1.6,4.1] # cenários desejável, com contenção e sem contenção
 
-z = 1 # teto estimado para o número de casos
+z = 1 # 100% da população
 
 for q in R0:
-    naive         = 2000000*z # população de Manaus: ~ 2 milhões de habitantes
-    week       = 0
+    naive         = 2000000*z   # vulneráveis
+    week          = 0
     sick          = 0
     hospitalized  = 0
-    recovered     = 0
-    new_cases    = 0
-
-    # flag_naive    = False
+    recovered     = 0           # imunes
+    new_cases     = 0
 
     while (naive > 0 or hospitalized > 1): # enquanto houver vuneráveis ou hospitalizados
         if naive > 0:
@@ -32,25 +30,23 @@ for q in R0:
             if new_cases > naive:
                 new_cases = naive
 
-            # if flag_naive:
-            #     naive = 0
-
             c = naive - recovered
             if c > 0: # número de vulneráveis
                 naive -= recovered
             else:
                 naive = 0
-                # flag_naive = True # tratamento para o caso em que o número de recovered é maior que o número de vulneráveis
 
 
         else:
             new_cases = 0
         
         hospitalized = round(sick*.05)
+        # -- liberação de leitos por óbito
         if week > 1:
-            hospitalized -= round(pow(q,week-3)*.03) # liberação de leitos por óbito
+            hospitalized -= round(pow(q,week-3)*.03) 
             if hospitalized < 0:
                 hospitalized = 0
+        # --
         recovered = round(sick*.95)
         sick -= recovered
         sick += new_cases
